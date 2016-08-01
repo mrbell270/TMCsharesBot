0e2377181e6e3da51846bd6e07b3abe6ed719935
Иван Скрыпников < >
2016-08-01 03:03:11 +0300

    reset added

--- bot.py
+++ bot.py
@@ -8,10 +8,10 @@ token = '268361464:AAG5gx_qSIZG7SRi-48gl4WLkw7XKXM6B_U'
 bot = telebot.TeleBot(token)
 
 shares = {
-    'Daniil':25,
-    'Damir':25,
-    'Иван':25,
-    'Said':25
+    'Daniil':0,
+    'Damir':0,
+    'Иван':0,
+    'Said':0
 }
 
 votesBy = {
@@ -35,28 +35,83 @@ votesFor = {
     'Said': set()
 }
 
+help_string = '''
+/% : Проценты.
+/? : Голоса.
+/- <Имя> : Голос против.
+/+ <Имя> : Голос за.
+/сброс, /reset : Сброс голосов.
+'''
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    help_message = '/% - Проценты.\n/? - Голоса.\n/- Имя - Голос против.\n/+ Имя - Голос за.'
-    bot.send_message(message.chat.id, help_message)
+@bot.message_handler(commands=['start'])
+def send_start(message):
+    if message.from_user.first_name == 'Иван':
+        shares = {
+            'Daniil':25,
+            'Damir':25,
+            'Иван':25,
+            'Said':25
+        }
+        help_message = 'Проценты уравнены\n' + help_string
+        bot.send_message(message.chat.id, help_message)
 
 
-@bot.message_handler(content_types=["text"], regexp=r'^/[?]$')
-def vote(message):
-    result = ''
-    for name in votes.keys():
-      result += name
-      result += ' - '
-      result += diciper(votes[name])
-      result += '\n'
-    bot.send_message(message.chat.id, result)
-
-def diciper(list):
-    return len(list)
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.send_message(message.chat.id, help_string)
 
 
-@bot.message_handler(content_types=["text"], regexp=r'^/%$')
+@bot.message_handler(content_types=["text"], regexp=r'^/-[ ]*[a-zA-Zа-яА-Я]+$')
+def downvote(message):
+    name = message.text[3:]
+    if name in shares.keys():
+        bot.send_message(
+          message.chat.id,
+          "{} votes against {}".format(message.from_user.first_name, name)
+        )
+        votesBy[message.from_user.first_name].add(name)
+        votesAgainst[name].add(message.from_user.first_name)
+        if len(votesAgainst[name]) == 3:
+            votesAgainst[name] = set()
+            shares[name] -= 3
+            for voter in shares.keys():
+                if not voter == name:
+                    shares[voter] += 1
+                    votesBy[voter].remove(name)
+
+
+@bot.message_handler(content_types=["text"], regexp=r'^/[+][ ]*[a-zA-Zа-яА-Я]+$')
+def upvote(message):
+    name = message.text[3:]
+    if name in shares.keys():
+        bot.send_message(
+          message.chat.id,
+          "{} votes for {}".format(message.from_user.first_name, name)
+        )
+        votesBy[message.from_user.first_name].add(name)
+        votesFor[name].add(message.from_user.first_name)
+        if len(votesFor[name]) == 4:
+            votesFor[name] = set()
+            shares[name] += 3
+            for voter in shares.keys():
+                if not voter == name:
+                    shares[voter]-= 1
+                    votesBy[voter].remove(name)
+
+
+@bot.message_handler(commands=['reset', 'сброс'])
+def devote(message):
+    name = message.from_user.first_name
+    votesBy[name] = set()
+    for each in votesFor.keys():
+        if name in votesFor[each]:
+            votesFor[each].remove(name)
+    for each in votesAgainst.keys():
+        if name in votesAgainst[each]:
+            votesAgainst[each].remove(name)
+
+
+@bot.message_handler(commands=['%'])
 def percentage(message):
     result = ''
     for name in shares.keys():
@@ -67,44 +122,27 @@ def percentage(message):
     bot.send_message(message.chat.id, result)
 
 
-@bot.message_handler(content_types=["text"], regexp=r'^/- [a-zA-Zа-яА-Я]+$')
-def downvote(message):
-    name = message.text[3:]
-    if name in shares.keys():
-        bot.send_message(message.chat.id, name)
-        votesBy[message.from_user.first_name].add(name)
-        votesAgainst[name].add(message.from_user.first_name)
-        if len(votesAgainst[name]) == 3:
-            votesAgainst[name] = set()
-            shares[name] -= 3
-            for voter in shares.keys():
-                print(name, voter)
-                if not voter == name:
-                    print(name, voter)
-                    shares[voter] += 1
-                    votesBy[voter].remove(name)
-        print(votesBy[message.from_user.first_name])
-        print(votesAgainst[name])
-
-
-@bot.message_handler(content_types=["text"], regexp=r'^/[+] [a-zA-Zа-яА-Я]+$')
-def upvote(message):
-    name = message.text[3:]
-    if name in shares.keys():
-        bot.send_message(message.chat.id, name)
-        votesBy[message.from_user.first_name].add(name)
-        votesFor[name].add(message.from_user.first_name)
-        if len(votesFor[name]) == 3:
-            votesFor[name] = set()
-            shares[name] += 3
-            for voter in shares.keys():
-                print(name, voter)
-                if not voter == name:
-                    print(name, voter)
-                    shares[voter]-= 1
-                    votesBy[voter].remove(name)
-        print(votesBy[message.from_user.first_name])
-        print(votesFor[name])
+@bot.message_handler(commands=['?'])
+def votesInfo(message):
+    name = message.from_user.first_name
+    result = name
+    result += "'s votes:\n"
+    for vote in votesBy[name]:
+      result += vote
+      result += '\n'
+    result += 'Votes against '
+    result += name
+    result += ':\n'
+    for vote in votesAgainst[name]:
+      result += vote
+      result += '\n'
+    result += 'Votes for '
+    result += name
+    result += ':\n'
+    for vote in votesFor[name]:
+      result += vote
+      result += '\n'
+    bot.send_message(message.chat.id, result)
     
     
 if __name__ == '__main__':
